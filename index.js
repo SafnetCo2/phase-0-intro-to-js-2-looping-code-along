324f7bc87e569569e90d9290e1f93d16baf90894
nzi19001@byui.edu <nzi19001@byui.edu>
2023-12-18 19:25:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solutions in this file
-function writeCards(name, string) {
+function writeCards(name, event) {
     const newArray = [];
     for (let i = 0; i < name.length; i++) {
-        newArray.push(`Thank you, ${name[i]}, for the wonderful ${string} gift!`);
+        newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
     }
     return newArray;
 }
